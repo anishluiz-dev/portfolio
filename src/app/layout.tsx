9ef3cbbf38d65f7a15643966457325a1964a1895
SorisAnish17 <anish.soris@gmail.com>
2025-12-18 22:44:09 +0530

    updated the meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
-"use client";
-
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import theme from "../theme/index";
+import { Metadata } from "next";
 import { MainLayOut } from "../layout/main-layout";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Soris | Full-Stack Web Developer",
+    template: "%s | Soris",
+  },
+  description:
+    "Soris is a full-stack web developer specializing in modern web applications using React, Next.js, and scalable backend technologies. View projects, skills, and experience.",
+};
 
 export default function RootLayout({
   children,
@@ -11,11 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Soris dev</title>
       <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <MainLayOut>{children}</MainLayOut>
-        </ThemeProvider>
+        <MainLayOut>{children}</MainLayOut>
       </body>
     </html>
   );
--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import { Container } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import { Navbar } from "../presentation/nav-bar";
 import { Footer } from "../presentation/footer";
+import theme from "../theme/index";
 
 interface IMainLayOutProps {
   children: React.ReactNode;
@@ -13,9 +15,12 @@ const MainLayOut: React.FC<IMainLayOutProps> = (props) => {
   const { children } = props;
   return (
     <React.Fragment>
-      <Navbar />
-      <Container>{children}</Container>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Container>{children}</Container>
+        <Footer />
+      </ThemeProvider>
     </React.Fragment>
   );
 };
