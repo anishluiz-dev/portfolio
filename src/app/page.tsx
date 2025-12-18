a1eb68b5281ec5c389108e52f9463552f7aef1fc
SorisAnish17 <anish.soris@gmail.com>
2025-12-18 22:23:38 +0530

    update the footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Skills } from "../presentation/skills";
 import { Experience } from "../presentation/experience";
 import { WorkHighlights } from "../presentation/work-highlights";
 import { Contact } from "../presentation/contact";
-import { Footer } from "../presentation/footer";
 
 export default function Home() {
   return (
@@ -15,7 +14,6 @@ export default function Home() {
       <Experience />
       <WorkHighlights />
       <Contact />
-      <Footer />
     </div>
   );
 }
--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Container } from "@mui/material";
 import { Navbar } from "../presentation/nav-bar";
+import { Footer } from "../presentation/footer";
 
 interface IMainLayOutProps {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ const MainLayOut: React.FC<IMainLayOutProps> = (props) => {
     <React.Fragment>
       <Navbar />
       <Container>{children}</Container>
+      <Footer />
     </React.Fragment>
   );
 };
