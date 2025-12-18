08b20ee29e4b2a1bf64c9fc230a14724b0fe5130
SorisAnish17 <anish.soris@gmail.com>
2025-12-18 22:48:05 +0530

    removed the title tag in main theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <title>Soris dev</title>
       <body>
         <MainLayOut>{children}</MainLayOut>
       </body>
